e6c08d225bdba3e6af93ef446887faa1a8cd0750
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-05-18 19:50:30 +0300

    fix: refactored

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,30 +3,27 @@ import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 import Logo from '@/shared/assets/logo.png';
 import AppLink from '@/shared/ui/AppLink/AppLink';
 
-export const Navbar = () => {
-    
-    return ( 
-        <div className='navbar'>
-            <AppLink 
-                to={RoutePath.home}
-                className='logo-link'
-            >
-                <img src={Logo} className="logo" alt="HaHaBoard logo" />
-            </AppLink>
+export const Navbar = () => ( 
+    <div className='navbar'>
+        <AppLink 
+            to={RoutePath.home}
+            className='logo-link'
+        >
+            <img src={Logo} className="logo" alt="HaHaBoard logo" />
+        </AppLink>
         
-            <AppLink 
-                to={RoutePath.home}
-            >
-                Home
-            </AppLink>
+        <AppLink 
+            to={RoutePath.home}
+        >
+            Home
+        </AppLink>
 
-            <AppLink 
-                to={RoutePath.about}
-            >
-                About
-            </AppLink>
+        <AppLink 
+            to={RoutePath.about}
+        >
+            About
+        </AppLink>
 
-        </div>
-    );
-};
+    </div>
+);
     
